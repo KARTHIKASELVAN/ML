36e0a98f84c0dbda80b92331cd121728b3879fee
KARTHIKASELVAN <117349561+KARTHIKASELVAN@users.noreply.github.com>
2024-05-15 12:29:32 +0530

    Update EX4.py

--- EX4.py
+++ EX4.py
@@ -1,109 +1,48 @@
-import streamlit as st
-import csv
-import random
-import math
+import pandas as pd
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-def loadCsv(file):
-    lines = csv.reader(file)
-    dataset = list(lines)
-    for i in range(len(dataset)):
-        # converting strings into numbers for processing
-        dataset[i] = [float(x) for x in dataset[i]]
-    return dataset
+# Load the dataset
+data = pd.read_csv("/content/drive/MyDrive/FDSA PROGRAM/DATASET/tennisdata.csv")
+print("The first 5 values of data are:\n", data.head())
 
-def splitDataset(dataset, splitRatio):
-    trainSize = int(len(dataset) * splitRatio)
-    trainSet = []
-    copy = list(dataset)
-    while len(trainSet) < trainSize:
-        index = random.randrange(len(copy))
-        trainSet.append(copy.pop(index))
-    return [trainSet, copy]
+# Obtain train data and train output
+X = data.iloc[:, :-1]
+print("\nThe first 5 values of train data are:\n", X.head())
 
-def separateByClass(dataset):
-    separated = {}
-    for vector in dataset:
-        classValue = vector[-1]
-        if classValue not in separated:
-            separated[classValue] = []
-        separated[classValue].append(vector)
-    return separated
+y = data.iloc[:, -1]
+print("\nThe first 5 values of train output are:\n", y.head())
 
-def mean(numbers):
-    return sum(numbers) / float(len(numbers))
+# Convert categorical features to numerical values
+le_outlook = LabelEncoder()
+X['Outlook'] = le_outlook.fit_transform(X['Outlook'])
 
-def stdev(numbers):
-    avg = mean(numbers)
-    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
-    return math.sqrt(variance)
+le_Temperature = LabelEncoder()
+X['Temperature'] = le_Temperature.fit_transform(X['Temperature'])
 
-def summarize(dataset):
-    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
-    del summaries[-1]
-    return summaries
+le_Humidity = LabelEncoder()
+X['Humidity'] = le_Humidity.fit_transform(X['Humidity'])
 
-def summarizeByClass(dataset):
-    separated = separateByClass(dataset)
-    summaries = {}
-    for classValue, instances in separated.items():
-        summaries[classValue] = summarize(instances)
-    return summaries
+le_Windy = LabelEncoder()
+X['Windy'] = le_Windy.fit_transform(X['Windy'])
 
-def calculateProbability(x, mean, stdev):
-    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
-    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+print("\nNow the train data is:\n", X.head())
 
-def calculateClassProbabilities(summaries, inputVector):
-    probabilities = {}
-    for classValue, classSummaries in summaries.items():
-        probabilities[classValue] = 1
-        for i in range(len(classSummaries)):
-            mean, stdev = classSummaries[i]
-            x = inputVector[i]
-            probabilities[classValue] *= calculateProbability(x, mean, stdev)
-    return probabilities
+le_PlayTennis = LabelEncoder()
+y = le_PlayTennis.fit_transform(y)
 
-def predict(summaries, inputVector):
-    probabilities = calculateClassProbabilities(summaries, inputVector)
-    bestLabel, bestProb = None, -1
-    for classValue, probability in probabilities.items():
-        if bestLabel is None or probability > bestProb:
-            bestProb = probability
-            bestLabel = classValue
-    return bestLabel
+print("\nNow the train output is:\n", y)
 
-def getPredictions(summaries, testSet):
-    predictions = []
-    for vector in testSet:
-        result = predict(summaries, vector)
-        predictions.append(result)
-    return predictions
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for i in range(len(testSet)):
-        if testSet[i][-1] == predictions[i]:
-            correct += 1
-    return (correct / float(len(testSet))) * 100.0
+# Initialize and train the classifier
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
-def main():
-    st.title('Naive Bayes Classifier')
-    
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    if uploaded_file is not None:
-        dataset = loadCsv(uploaded_file)
-        splitRatio = st.slider('Select split ratio for training set', 0.1, 0.9, 0.67)
-        
-        trainingSet, testSet = splitDataset(dataset, splitRatio)
-        st.write('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
-        
-        # prepare model
-        summaries = summarizeByClass(trainingSet)
-        
-        # test model
-        predictions = getPredictions(summaries, testSet)
-        accuracy = getAccuracy(testSet, predictions)
-        st.write('Accuracy of the classifier is : {0}%'.format(accuracy))
-
-if _name_ == "_main_":
-    main()
+# Evaluate the classifier
+accuracy = accuracy_score(classifier.predict(X_test), y_test)
+print("Accuracy is:", accuracy)
